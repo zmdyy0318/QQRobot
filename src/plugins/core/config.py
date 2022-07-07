5e171a0cb59e14590e58a7c947e0d41f4e9cbf8a
zmdyy0318 <zmdyy0318@hotmail.com>
2022-07-07 09:48:37 +0800

    chore:配置文件增加校验

--- src/plugins/core/config.py
+++ src/plugins/core/config.py
@@ -4,6 +4,8 @@ from pydantic import BaseSettings
 class Config(BaseSettings):
     plugin_names: list
     superusers: list
+    onebot_ws_urls: list
+    onebot_access_token: str
 
     class Config:
         extra = "ignore"
